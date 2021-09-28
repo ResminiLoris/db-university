ff1381615cb44cc31632eccf164e4f6688a540ac
ResminiLoris <resminiloris@gmail.com>
2021-09-28 17:59:30 +0200

    query_6

--- queries.sql
+++ queries.sql
@@ -17,3 +17,10 @@ WHERE YEAR(`date_of_birth`) < (YEAR(CURDATE()) - 30);
 SELECT * 
 FROM `students` 
 WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURDATE()) > 30; 
+
+--Selezionare tutti i corsi di laurea magistrale (38) --
+SELECT * 
+FROM `degrees` 
+WHERE `level` = 'magistrale';
+
+
