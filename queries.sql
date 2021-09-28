6c0297295a4c7e7b0b258d5c5dd3d377344575e9
ResminiLoris <resminiloris@gmail.com>
2021-09-28 18:07:08 +0200

    query_4

--- queries.sql
+++ queries.sql
@@ -28,3 +28,9 @@ SELECT *
 FROM `exams` 
 WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
 
+-- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286) --
+SELECT * 
+FROM `courses` 
+WHERE `period`='I semestre' AND `year` = 1;
+
+
