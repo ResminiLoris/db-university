e09daf9e02d86b1cc26f8827ca80f958d59dbe47
ResminiLoris <resminiloris@gmail.com>
2021-09-28 17:35:42 +0200

    query_3

--- queries.sql
+++ queries.sql
@@ -6,4 +6,15 @@ WHERE `date_of_birth` >= '1990-01-01' AND `date_of_birth` <= '1990-12-31';
 --Selezionare tutti i corsi che valgono più di 10 crediti--
 SELECT * 
 FROM `courses` 
-WHERE `cfu` > 10
+WHERE `cfu` > 10;
+
+-- Selezionare tutti gli studenti che hanno più di 30 anni--
+--SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < (YEAR(CURDATE()) - 30)--
+SELECT * 
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURDATE()) > 30;
+
+--oppure--
+SELECT * 
+FROM `students` 
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURDATE()) > 30;
